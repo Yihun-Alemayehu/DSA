256d4311b6a6cc8b382e8fd13919788a36b02c65
Yihun Alemayehu <yankure01@gmail.com>
2023-12-02 19:36:27 +0300

    recursive

--- reverse_array.cpp
+++ reverse_array.cpp
@@ -2,7 +2,9 @@
 
 using namespace std;
 
-void reverseArray(int arr[], int start, int end)
+// Iterative way
+
+/*void reverseArray(int arr[], int start, int end)
 {
     while (start < end)
     {
@@ -22,8 +24,7 @@ void printArray(int arr[], int n)
     }
 }
 
-int main()
-{
+int main(){
     int arr[] = {
         1,
         2,
@@ -33,6 +34,37 @@ int main()
         6,
         7,
     };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    reverseArray(arr, 0, n - 1);
+    printArray(arr, n);
+}*/
+
+// Recursive way
+
+void reverseArray(int arr[], int start, int end)
+{
+    if (start >= end)
+        return;
+
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+
+    reverseArray(arr, start + 1, end - 1);
+}
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << endl;
+    }
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
     int n = sizeof(arr) / sizeof(arr[0]);
 
     reverseArray(arr, 0, n - 1);
