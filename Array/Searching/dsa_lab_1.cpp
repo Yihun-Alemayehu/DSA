0563f405cf16dfb6c53287fee0fcf4a552aca19e
Yihun Alemayehu <yankure01@gmail.com>
2023-12-14 17:23:59 +0300

    lab-2 commit

---
+++ Array/Searching/dsa_lab_1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+   int n=8; 
+   for(int i=1; i<=n; i++){
+    cout << "hello world!!!\n ";
+   }
+   return 0;
+    /*   So, the time complexity is linear: O(n) i.e. every time, a linear
+          amount of time is required to execute code.*/
+}
---
+++ Array/Searching/linear.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[10], i, num, index;
+    cout << "Enter 5 Numbers: ";
+    for (i = 0; i < 5; i++)
+        cin >> arr[i];
+    cout << "\nEnter a Number to Search: ";
+    cin >> num;
+    for (i = 0; i < 5; i++)
+    {
+        if (arr[i] == num)
+        {
+            index = i;
+            break;
+        }
+    }
+    cout << "\nFound at Index No." << index;
+    cout << endl;
+    return 0;
+}
